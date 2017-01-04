520c8f1f85a26a8cfd71fc1f70308edb5f9755e3
yatindrarao <yatindrarao.rao@gmail.com>
2017-01-04 21:19:18 +0530

    Changes to app.run()

--- application.py
+++ application.py
@@ -426,5 +426,5 @@ def user_signed_in():
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.debug = False
+    app.run()
